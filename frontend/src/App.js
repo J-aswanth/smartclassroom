42b8c31dfe31c95e6e8a042753143ff49c1f2597
J-aswanth <128897545+J-aswanth@users.noreply.github.com>
2025-05-16 17:17:59 +0530

    frontend update

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Mainpage from './components/mainpage.js'
+
+function App() {
+  return (
+    <div>     
+      <Mainpage />
+    </div>
+      
+  );
+}
+
+export default App;
